c229522c2beb3486983f6c7861355ff40223b489
Guilherme da Nova <guibiluca92@gmail.com>
2024-06-28 09:48:47 -0300

    Botão Salvar implementado

--- src/ProdutosDAO.java
+++ src/ProdutosDAO.java
@@ -1,34 +1,51 @@
 
-
 import java.sql.PreparedStatement;
 import java.sql.Connection;
-import javax.swing.JOptionPane;
 import java.sql.ResultSet;
+import java.sql.SQLException;
 import java.util.ArrayList;
 
-
 public class ProdutosDAO {
-    
+
     Connection conn;
-    PreparedStatement prep;
-    ResultSet resultset;
+    PreparedStatement st;
+    ResultSet rs;
     ArrayList<ProdutosDTO> listagem = new ArrayList<>();
-    
-    public void cadastrarProduto (ProdutosDTO produto){
-        
-        
-        //conn = new conectaDAO().connectDB();
-        
-        
+
+    public int cadastrarProduto(ProdutosDTO produto) {
+
+        conn = new conectaDAO().connectDB();
+        if (conn == null) {
+        return -1;
+        }
+
+        int status;
+
+        try {
+
+            st = conn.prepareStatement("INSERT INTO produtos (nome, valor, status) VALUES (?,?,?)");
+            st.setString(1, produto.getNome());
+            st.setInt(2, produto.getValor());
+            st.setString(3, produto.getStatus());
+            status = st.executeUpdate();
+            return status;
+        } catch (SQLException ex) {
+            return ex.getErrorCode();
+        }
     }
-    
-    public ArrayList<ProdutosDTO> listarProdutos(){
-        
+
+    public ArrayList<ProdutosDTO> listarProdutos() {
+
         return listagem;
     }
-    
-    
-    
-        
-}
 
+    
+    public void desconectar() {
+        try {
+            this.conn.close();
+        } catch (SQLException ex) {
+            System.out.print("erro ao desconectar " + ex.getMessage());
+        }
+    }
+    
+}
